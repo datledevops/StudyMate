5d9a8c01288fe3cd31ae3efafcf23e0ccc7b64f2
LeDat1709 <letandat1709@gmail.com>
2026-08-20 20:48:29 +0700

    chore: initial project setup

---
+++ StudyMate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudyMate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
